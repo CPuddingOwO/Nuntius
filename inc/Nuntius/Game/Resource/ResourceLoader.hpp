6edae534d552b9400ee0e06902dab891b9f68bce
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2024-12-06 17:53:33 +0800

    资源管理器

---
+++ inc/Nuntius/Game/Resource/ResourceLoader.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <Nuntius/API.hpp>
+#include <Nuntius/Game/Resource/Types.hpp>
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <unordered_map>
+
+namespace nt::res {
+    class NT_API ResourceLoader {
+    public:
+        template<typename T>
+        void Add(const std::string& id, const std::string& path) {
+            std::shared_ptr<T> resource = std::make_shared<T>();
+            resource->Load(path);
+            resources.emplace(id, resource);
+        }
+
+        template<typename T>
+        void Add(const types::ResourceInfo& info) {
+            std::shared_ptr<T> resource = std::make_shared<T>();
+            resource->Load(info.path);
+            resources.emplace(info.id, resource);
+        }
+
+        std::shared_ptr<types::Resource> Get(const std::string& id) {
+            auto it = resources.find(id); // ERROR
+            if (it != resources.end()) {
+                return it->second;
+            }
+            return nullptr;
+        }
+
+    private:
+        std::unordered_map<std::string, std::shared_ptr<types::Resource>> resources = {};
+    };
+}
---
+++ src/Game/Resource/ResourceLoader.cpp
@@ -0,0 +1,5 @@
+#include <Nuntius/Game/Resource/ResourceLoader.hpp>
+
+namespace nt::res {
+
+}
