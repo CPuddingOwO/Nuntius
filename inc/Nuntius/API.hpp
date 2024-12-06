e49bc0eb2f9f2263c328bb3b1261ef88dafd1897
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2024-12-06 17:52:59 +0800

    Exports API

--- inc/Nuntius/API.hpp
+++ inc/Nuntius/API.hpp
@@ -9,3 +9,17 @@
 #else
     #define NT_API __attribute__((visibility("default")))
 #endif
+
+//#ifndef nuntius_STATIC
+//    #if NUNTIUS_EXPORTS && (defined(_MSC_VER) || defined(__MINGW32__))
+//        #define NT_API __declspec(dllexport)
+//    #elif NUNTIUS_EXPORTS
+//        #define NT_API __attribute__((__visibility__("default")))
+//    #elif defined _MSC_VER
+//        #define NT_API __declspec(dllimport)
+//    #else
+//        #define NT_API
+//    #endif
+//#else
+//    #define NT_API
+//#endif
